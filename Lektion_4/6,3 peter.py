1a807b27cfebe94c471c16e098bdbd01f2a3e80d
Peter Sjöberg <p-sjoberg@hotmail.com>
2023-09-06 08:53:01 +0200

    okej

--- Lektion_4/6,3 peter.py
+++ Lektion_4/6,3 peter.py
@@ -1,9 +1,16 @@
 user_input = input("Skriv en text här: ")
+input_spacefree = ""
 clean_string = ""
 mellanslag = " "
 
 for bokstav in user_input:
-    if bokstav.lower() in mellanslag:
+    if bokstav.lower() not in mellanslag:
         clean_string += bokstav
+        input_spacefree += bokstav
 
 print (clean_string)
+
+if clean_string == input_spacefree[::-1]:
+    print ("Palindrom")
+else:
+    print("icke palindrom")
